77484736512eb555efe75d95ed320d22be8e05cd
Dane Dalton <danedalton@sbcglobal.net>
2022-06-19 12:28:38 -0500

    Pseudo-code written

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tic-tac-toe
+# Tic Tac Toe
 A game of Tic Tac Toe played between two players in the console.
---
+++ tic_tac_toe.rb
@@ -0,0 +1,28 @@
+#Board
+#    |   |  
+# -----------
+#    |   |
+# -----------
+#    |   |
+
+#Initial UI
+  #Welcome message
+  #Random selection for who goes first
+
+#Display board with numbered rows and columns
+
+  #Check if there is an open space
+    #Play unless there are no spaces open
+      #Prompt a draw, ask if they would like to play a new game
+
+  #Ask which row they would like to pick, then column
+
+  #See if the spot is filled
+    #Prompt for another selection if the spot is filled
+
+  #Fill the spot
+
+  #Check if the game is over
+    #If done, prompt the winner
+      #Ask to play again
+    #else, switch turns
