9d58271a690270423db6d5173ee7a7fa51af9424
Dane Dalton <danedalton@sbcglobal.net>
2022-06-19 13:38:01 -0500

    Basic board layout and storage for each of its items. NewGame class and Board module.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,7 @@
 #Board
-#    |   |  
-# -----------
-#    |   |
-# -----------
-#    |   |
+# _1_|_2_|_3_
+# _4_|_5_|_6_
+#  X | 8 | 9
 
 #Initial UI
   #Welcome message
@@ -26,3 +24,46 @@
     #If done, prompt the winner
       #Ask to play again
     #else, switch turns
+
+module GamePrompts
+  def player_choice(player)
+    puts "It is #{player}'s turn. Please pick a spot to play."
+    gets.chomp.to_i 
+  end
+end
+
+module Board
+  def current_board(items)
+    puts "_#{item[0]}_|_#{item[1]}_|_#{item[2]}_"
+    puts "_#{item[3]}_|_#{item[4]}_|_#{item[5]}_"
+    puts " #{item[6]} | #{item[7]} | #{item[8]} "
+  end
+end
+
+class NewGame
+  include GamePrompts
+  include Board
+
+  @@PLAYERS = ["Player 1", "Player 2"]
+
+  attr_reader :current_player
+
+  def initialize()
+    @current_player = ((rand(2) == 0)? @@PLAYERS[0]: @@PLAYERS[1])
+    @board_slots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  end
+
+  def prompt_current_player
+    puts "It is #{self.current_player}'s turn. Please pick a spot to play:"
+    gets.chomp.to_i
+  end
+
+  private
+    def swap_player
+      (@current_player == @@PLAYERS[0])? @current_player = @@PLAYERS[1]: @current_player = @@PLAYERS[0]
+    end
+end
+
+puts "Welcome! Grab a friend and get ready for some Tic Tac Toe!"
+new_game = NewGame.new()
+new_game.prompt_current_player
